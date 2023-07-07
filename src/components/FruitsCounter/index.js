77b16685eae3e14545b0b94e0405da322d1ab943
root <nikhil.marthala@gmail.com>
2023-07-07 06:05:22 +0000

    react-js-cp9-debuggingFruitsCounter

---
+++ src/components/FruitsCounter/index.js
@@ -0,0 +1,74 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class FruitsCounter extends Component {
+  // Here give the values to the state
+  state = {
+    mangoesCount: 4,
+    bananasCount: 5,
+  }
+
+  onClickEatBanana = () => {
+    this.setState(prevState => ({bananasCount: prevState.bananasCount + 1}))
+  }
+
+  onClickEatMango = () => {
+    this.setState(prevState => ({mangoesCount: prevState.mangoesCount + 1}))
+  }
+
+  render() {
+    // Here give the value as this.state
+    const {mangoesCount, bananasCount} = this.state
+    // Here change the all class to className
+    return (
+      <div className="app-container">
+        <div className="fruits-counter">
+          <h1 className="count-text">
+            Bob ate <span className="count">{mangoesCount}</span> mangoes
+            <span className="count">{bananasCount}</span> bananas
+          </h1>
+          <div className="counters-control-container">
+            <div className="counter-control">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/mango-img.png"
+                className="fruit-image"
+                { /* Here change the alt value*/ }
+                alt="mango"
+              />
+              <div className="button-container">
+                <button
+                  type="button"
+                  className="button"
+                  { /* Here change the onClick*/ }
+                  onClick={this.onClickEatMango}
+                >
+                  Eat Mango
+                </button>
+              </div>
+            </div>
+            <div className="counter-control">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/banana-img.png"
+                alt="banana"
+                className="fruit-image"
+              />
+              <div className="button-container">
+                <button
+                  type="button"
+                  className="button"
+                  { /* Here change the onClick*/ }
+                  onClick={this.onClickEatBanana}
+                >
+                  Eat Banana
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FruitsCounter
